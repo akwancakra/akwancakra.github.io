ce46e9d21654b004797f36831b4fd2fa47da8046
akwancakra <akwancakra20@gmail.com>
2024-04-06 08:22:26 +0700

    📟 remake portfolio

---
+++ public/js/animate-show.js
@@ -0,0 +1,13 @@
+ScrollReveal({
+    reset: true,
+});
+
+ScrollReveal().reveal(".show-from-bottom", {
+    origin: "bottom",
+    distance: "70px",
+    easing: "cubic-bezier(.37,.01,.74,1)",
+});
+
+ScrollReveal().reveal(".show-item", {
+    interval: 100,
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'class',
-  content: ["./public/**/*.{html,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    darkMode: "class",
+    content: ["./public/**/*.{html,js}"],
+    theme: {
+        fontFamily: {
+            space: ['"Space Grotesk"', "sans-serif"],
+        },
+        extend: {
+            textColors: {
+                black: "#333333",
+            },
+            borderRadius: {
+                15: "15px",
+                20: "20px",
+            },
+            keyframes: {
+                increaseHeight: {
+                    "0%": { height: "72px" },
+                    "50%": { height: "120px" },
+                    "100%": { height: "72px" },
+                },
+            },
+            animation: {
+                height: "increaseHeight 1.7s infinite",
+            },
+        },
+    },
+    plugins: [],
+};
